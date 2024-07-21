6ba7aaa7243b494cd9e7fbffa96ecb8606b19f4d
Krzysztof Hankiewicz @kjhank <kjhank@gmail.com>
2024-07-21 14:46:48 +0200

    carousel logic

--- src/scripts/carousel.ts
+++ src/scripts/carousel.ts
@@ -4,15 +4,7 @@ const setupCarousel = (carousel: Element) => {
   const items = carousel.querySelectorAll('.item');
   const buttons = carousel.querySelectorAll('carousel-button');
 
-  // const handleClick = ({ target }: Event) => {
-
-  // };
-
-  // console.log(items, buttons);
-
-  setInterval(() => {
-    // items.forEach((item) => item.setAttribute('data-inactive', ''));
-    // items[0].removeAttribute('data-inactive');
+  const initTimeout = () => setInterval(() => {
     const currentActive = Array.from(items).find((item) => item.hasAttribute('data-active'));
 
     if (!currentActive) return;
@@ -32,9 +24,56 @@ const setupCarousel = (carousel: Element) => {
       nextActive?.removeAttribute('data-inactive');
       nextActive?.setAttribute('data-active', '');
     }
-
-    console.log('ping');
   }, INTERVAL);
+
+  let timeoutId = initTimeout();
+
+  const handleSteps = (event: Event | KeyboardEvent) => {
+    const { currentTarget } = event;
+    if (!currentTarget || !(currentTarget instanceof Element)) return;
+    if (event instanceof KeyboardEvent && event.key !== 'Enter') return;
+
+    const direction = currentTarget?.getAttribute('direction');
+
+    const currentActive = Array.from(items).find((item) => item.hasAttribute('data-active'));
+
+    if (!currentActive) return;
+
+    const isFirst = currentActive === [...items].at(0);
+    const isLast = currentActive === [...items].at(-1);
+
+    items.forEach((item) => item.removeAttribute('data-active'));
+    items.forEach((item) => item.setAttribute('data-inactive', ''));
+
+    const nextActive = currentActive.nextElementSibling;
+    const previousActive = currentActive.previousElementSibling;
+
+    clearInterval(timeoutId);
+    timeoutId = initTimeout();
+
+    if (direction === 'previous' && isFirst) {
+      [...items].at(-1)?.setAttribute('data-active', '');
+      [...items].at(-1)?.removeAttribute('data-inactive');
+      return;
+    }
+
+    if (direction === 'next' && isLast) {
+      [...items].at(0)?.setAttribute('data-active', '');
+      [...items].at(0)?.removeAttribute('data-inactive');
+      return;
+    }
+
+    if (direction === 'previous') {
+      previousActive?.setAttribute('data-active', '');
+      previousActive?.removeAttribute('data-inactive');
+    } else {
+      nextActive?.setAttribute('data-active', '');
+      nextActive?.removeAttribute('data-inactive');
+    }
+  };
+
+  buttons.forEach((button) => button.addEventListener('click', handleSteps));
+  buttons.forEach((button) => button.addEventListener('keydown', handleSteps));
 };
 
 export const initCarousels = () => {
